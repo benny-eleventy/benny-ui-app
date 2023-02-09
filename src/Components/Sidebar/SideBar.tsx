14cb3a458a11a52e00b6cdaa6b07ac81e957ab95
sgrmhdk <31389818+SagarMahadik@users.noreply.github.com>
2023-02-09 10:49:44 +0530

    added npm read token

---
+++ src/Components/Sidebar/SideBar.tsx
@@ -0,0 +1,21 @@
+import { FlexStartCenterAlignedColumnContainer } from "@benny-eleventy/benny-ui";
+import React from "react";
+
+const SideBar = () => {
+	const sidebarHeaders = [
+		"Styled-Components",
+		"Styles",
+		"Themes",
+		"Icons",
+		"Logos",
+		"Components",
+	];
+
+	return (
+		<FlexStartCenterAlignedColumnContainer>
+			SideBar
+		</FlexStartCenterAlignedColumnContainer>
+	);
+};
+
+export default SideBar;
